233d2419e231fb2e94398e2e06e2ecaaa26d5d74
Arne Hormann <arnehormann@gmail.com>
2013-06-03 12:36:10 +0200

    inlined helper methods, got rid of unused errors

--- inspect.go
+++ inspect.go
@@ -14,9 +14,6 @@ var (
 )
 
 // internal error type
-// Used instead of import "errors" for two reasons:
-// - is used nowhere else, making it a good template for an AssignableTo assertion
-// - can be used in const
 type internalErr string
 
 func (e internalErr) Error() string {
@@ -26,9 +23,7 @@ func (e internalErr) Error() string {
 const (
 	errArgNil       = internalErr("argument must not be nil")
 	errArgWrongType = internalErr("argument was not *sql.Row or *sql.Rows")
-	errRowRows      = internalErr("'rows *sql.Rows' in sql.Row could not be read")
 	errRowRowsNil   = internalErr("'err' xor 'rows' in sql.Row must be nil")
-	errRowsRowsi    = internalErr("'rowsi driver.Rows' in sql.Rows could not be read")
 	errRowsRowsiNil = internalErr("'rowsi driver.Rows' in sql.Rows is nil")
 )
 
@@ -94,54 +89,34 @@ func init() {
 	}
 }
 
-// return rows from sql/*Row, if row or row.rows is nil, an error is returned.
-// Has to use unsafe to access unexported fields, but it's robust:
-// we checked the types and structure in init.
-func sqlRowsFromSqlRow(row *sql.Row) (*sql.Rows, error) {
-	if row == nil {
-		return nil, errArgNil
-	}
-	rowsPtr := (uintptr)((unsafe.Pointer)(row)) + offsetRowRows
-	rows := *(**sql.Rows)((unsafe.Pointer)(rowsPtr))
-	if rows == nil {
-		return nil, errRowRowsNil
-	}
-	return rows, nil
-}
-
-// return rowsi from sql/*Rows, if rows or rows.rowsi is nil an error is returned.
-// Has to use unsafe to access unexported fields, but it's robust:
-// we checked the types and structure in init.
-func driverRowsFromSqlRows(rows *sql.Rows) (driver.Rows, error) {
-	if rows == nil {
-		return nil, errArgNil
-	}
-	rowsiPtr := offsetRowsRowsi + (uintptr)((unsafe.Pointer)(rows))
-	if rowsiPtr == 0 {
-		return nil, errRowsRowsi
-	}
-	rowsi := *(*driver.Rows)((unsafe.Pointer)(rowsiPtr))
-	return rowsi, nil
-}
-
 // Inspect extracts the internal driver.Rows from sql.Row or sql.Rows.
 // This can be used by a driver to work around issue 5606 in legacy versions.
 func Inspect(sqlStruct interface{}) (interface{}, error) {
+	// All of this has to use unsafe to access unexported fields, but it's robust:
+	// we checked the types and structure in init.
 	if sqlStruct == nil {
 		return nil, errArgNil
 	}
 	var rows *sql.Rows
 	switch v := sqlStruct.(type) {
 	case *sql.Row:
-		var err error
-		rows, err = sqlRowsFromSqlRow(v)
-		if err != nil {
-			return nil, err
+		// extract rows from sql/*Row, if v.rows is nil, an error is returned.
+		rowsPtr := (uintptr)((unsafe.Pointer)(v)) + offsetRowRows
+		unsafeRows := *(**sql.Rows)((unsafe.Pointer)(rowsPtr))
+		if unsafeRows == nil {
+			return nil, errRowRowsNil
 		}
+		rows = unsafeRows
 	case *sql.Rows:
 		rows = v
 	default:
 		return errArgWrongType, nil
 	}
-	return driverRowsFromSqlRows(rows)
+	// return rowsi from sql/*Rows, if rows.rowsi is nil an error is returned.
+	rowsiPtr := offsetRowsRowsi + (uintptr)((unsafe.Pointer)(rows))
+	rowsi := *(*driver.Rows)((unsafe.Pointer)(rowsiPtr))
+	if rowsi == nil {
+		return nil, errRowsRowsiNil
+	}
+	return rowsi, nil
 }
